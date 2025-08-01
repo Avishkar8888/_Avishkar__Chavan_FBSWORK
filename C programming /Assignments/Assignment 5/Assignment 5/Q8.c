deccf702950040a35cad3b975d3ce8523e037100
Avishkar8888 <chavanavishkar98@gmail.com>
2025-08-01 20:43:25 +0530

    Add files via upload

---
+++ C programming /Assignments/Assignment 5/Assignment 5/Q8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main() {
+    int n = 4;
+    for(int i = 1; i <= n; i++) {
+        for(int j = i; j < n; j++) {
+            printf(" ");
+        }
+        for(int k = 1; k <= i; k++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    for(int i = n - 1; i >= 1; i--) {
+        for(int j = n; j > i; j--) {
+            printf(" ");
+        }
+        for(int k = 1; k <= i; k++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
